bb486db939427146b02ce935a009efd3ccdb7646
还行吧 <XiaoqingLee@users.noreply.github.com>
2021-11-06 11:35:54 +0800

    finish 5

---
+++ leetcodeproblems/0005_LongestPalindromicSubstring.go
@@ -0,0 +1,44 @@
+package leetcodeproblems
+
+func LongestPalindrome(s string) string {
+
+	longestLeftCharIndex := 0
+	longestRightCharIndex := 0
+	longestLength := 0
+
+	leftCharIndex := 0
+	rightCharIndex := 0
+	length := 0
+
+	charAsCenter := [...]bool{false, true} //false: 以字符的左边界作为中心, true: 以字符本身作为中心
+	for i := range s {
+		for _, charAsCenter := range charAsCenter {
+
+			if charAsCenter {
+				leftCharIndex = i - 1
+				rightCharIndex = i + 1
+			} else {
+				leftCharIndex = i - 1
+				rightCharIndex = i
+			}
+
+			for {
+				if leftCharIndex < 0 || rightCharIndex >= len(s) || s[leftCharIndex] != s[rightCharIndex] {
+					break
+				}
+				leftCharIndex -= 1
+				rightCharIndex += 1
+			}
+			leftCharIndex += 1
+			rightCharIndex -= 1
+
+			length = rightCharIndex - leftCharIndex + 1
+			if length > longestLength {
+				longestLeftCharIndex = leftCharIndex
+				longestRightCharIndex = rightCharIndex
+				longestLength = length
+			}
+		}
+	}
+	return s[longestLeftCharIndex : longestRightCharIndex+1]
+}
