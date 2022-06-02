d3b0f912eb76bc82a9c30361555eb0b2d3e9d934
还行吧 <XiaoqingLee@users.noreply.github.com>
2022-06-02 16:27:40 +0800

    finish lc 5

--- problems/lc0005_LongestPalindromicSubstring.go
+++ problems/lc0005_LongestPalindromicSubstring.go
@@ -1,46 +1,40 @@
 package problems
 
 func LongestPalindromicSubstring(s string) string {
+	if len(s) == 0 {
+		return s
+	}
 
-	//不考虑字符串s中包含字节数大于1的rune的情景
+	dp := make([][]bool, len(s))
+	for i := range dp {
+		dp[i] = make([]bool, len(s))
+	}
+	longestPalindromicSubstringLength := 0
+	longestPalindromicSubstring := ""
 
-	longestLeftCharIndex := 0
-	longestRightCharIndex := 0
-	longestLength := 0
-
-	leftCharIndex := 0
-	rightCharIndex := 0
-	length := 0
-
-	charAsCenter := [...]bool{false, true} //false: 以字符的左边界作为中心, true: 以字符本身作为中心
-	for i := range s {
-		for _, charAsCenter := range charAsCenter {
-
-			if charAsCenter {
-				leftCharIndex = i - 1
-				rightCharIndex = i + 1
-			} else {
-				leftCharIndex = i - 1
-				rightCharIndex = i
-			}
-
-			for {
-				if leftCharIndex < 0 || rightCharIndex >= len(s) || s[leftCharIndex] != s[rightCharIndex] {
+	populateDpTable := func() {
+		for subStringLen := 1; subStringLen <= len(s); subStringLen++ {
+			//子串左边界i（包含），有边界j（包含）
+			for i := range s {
+				j := i + (subStringLen - 1)
+				if j >= len(s) {
 					break
 				}
-				leftCharIndex -= 1
-				rightCharIndex += 1
-			}
-			leftCharIndex += 1
-			rightCharIndex -= 1
-
-			length = rightCharIndex - leftCharIndex + 1
-			if length > longestLength {
-				longestLeftCharIndex = leftCharIndex
-				longestRightCharIndex = rightCharIndex
-				longestLength = length
+				if i == j {
+					dp[i][j] = true
+				} else if i+1 == j {
+					dp[i][j] = s[i] == s[j]
+				} else {
+					dp[i][j] = s[i] == s[j] && dp[i+1][j-1]
+				}
+				if dp[i][j] && subStringLen > longestPalindromicSubstringLength {
+					longestPalindromicSubstring = s[i : j+1]
+					longestPalindromicSubstringLength = subStringLen
+				}
 			}
 		}
 	}
-	return s[longestLeftCharIndex : longestRightCharIndex+1]
+
+	populateDpTable()
+	return longestPalindromicSubstring
 }
