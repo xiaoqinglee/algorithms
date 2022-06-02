d1ddc09b527ee45c7f01ed5d1c3deb709476236a
还行吧 <XiaoqingLee@users.noreply.github.com>
2022-06-02 22:06:23 +0800

    finish lc 11 19 704

---
+++ problems/lc0011_ContainerWithMostWater.go
@@ -0,0 +1,34 @@
+package problems
+
+func ContainerWithMostWater(height []int) int {
+
+	min := func(a, b int) int {
+		if a < b {
+			return a
+		} else {
+			return b
+		}
+	}
+
+	if len(height) <= 1 {
+		panic("Invalid Input height")
+	}
+	leftIndex := 0
+	rightIndex := len(height) - 1
+	maxArea := 0
+	for {
+		if leftIndex == rightIndex {
+			break
+		}
+		area := (rightIndex - leftIndex) * min(height[leftIndex], height[rightIndex])
+		if area > maxArea {
+			maxArea = area
+		}
+		if height[leftIndex] < height[rightIndex] {
+			leftIndex += 1
+		} else {
+			rightIndex -= 1
+		}
+	}
+	return maxArea
+}
---
+++ problems/lc0011_ContainerWithMostWater.py
@@ -0,0 +1,16 @@
+def container_with_most_water(height: list[int]) -> int:
+    if len(height) <= 1:
+        raise "Invalid Input height"
+    left_index = 0
+    right_index = len(height) - 1
+    max_area = 0
+    while True:
+        if left_index == right_index:
+            break
+        area = (right_index - left_index) * min(height[left_index], height[right_index])
+        max_area = area if area > max_area else max_area
+        if height[left_index] < height[right_index]:
+            left_index += 1
+        else:
+            right_index -= 1
+    return max_area
