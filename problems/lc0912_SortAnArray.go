76d1f20fc7c83dcf660f8a4b7bb9710d7e30f7ad
还行吧 <XiaoqingLee@users.noreply.github.com>
2022-06-03 13:29:40 +0800

    finish 归并排

--- problems/lc0912_SortAnArray.go
+++ problems/lc0912_SortAnArray.go
@@ -52,7 +52,59 @@ func QuickSort(nums []int) []int {
 		midIndex := partition(leftIndex, rightIndex)
 		sort_(leftIndex, midIndex-1)
 		sort_(midIndex+1, rightIndex)
+	}
+	sort_(0, len(nums)-1)
+	return nums
+}
 
+func MergeSort(nums []int) []int {
+
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(nums), func(i, j int) {
+		nums[i], nums[j] = nums[j], nums[i]
+	})
+
+	if len(nums) == 0 {
+		return nums
+	}
+
+	container := make([]int, len(nums), len(nums))
+
+	merge := func(leftIndex, midIndex, rightIndex int) {
+		copy(container[leftIndex:rightIndex+1], nums[leftIndex:rightIndex+1])
+		leftPointer, rightPointer, mergingPointer := leftIndex, midIndex+1, leftIndex
+
+		for leftPointer <= midIndex && rightPointer <= rightIndex {
+			if container[leftPointer] <= container[rightPointer] {
+				nums[mergingPointer] = container[leftPointer]
+				leftPointer += 1
+			} else {
+				nums[mergingPointer] = container[rightPointer]
+				rightPointer += 1
+			}
+			mergingPointer += 1
+		}
+		for leftPointer <= midIndex {
+			nums[mergingPointer] = container[leftPointer]
+			leftPointer += 1
+			mergingPointer += 1
+		}
+		for rightPointer <= rightIndex {
+			nums[mergingPointer] = container[rightPointer]
+			rightPointer += 1
+			mergingPointer += 1
+		}
+	}
+
+	var sort_ func(int, int)
+	sort_ = func(leftIndex, rightIndex int) {
+		if leftIndex >= rightIndex {
+			return
+		}
+		midIndex := (leftIndex + rightIndex) / 2
+		sort_(leftIndex, midIndex)
+		sort_(midIndex+1, rightIndex)
+		merge(leftIndex, midIndex, rightIndex)
 	}
 	sort_(0, len(nums)-1)
 	return nums
