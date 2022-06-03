4faf2412d4b1dd71397f652d4ca467bf17670d65
还行吧 <XiaoqingLee@users.noreply.github.com>
2022-06-04 00:17:54 +0800

    finish lc 101

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from problems.lc0015_3Sum import three_sum
+from problems.lc0912_SortAnArray import heap_sort
 
 if __name__ == "__main__":
-    nums = [0,0,0,0,0]
-    print(three_sum(nums))
+    nums = [3,4,5,2,1,6,4,8]
+    print(heap_sort(nums))
