6919d8aee2553fe85e4b3f82770c4a8dd006fdf7
还行吧 <XiaoqingLee@users.noreply.github.com>
2022-06-07 22:59:22 +0800

    daily

--- problems/lc0150_EvaluateReversePolishNotation.go
+++ problems/lc0150_EvaluateReversePolishNotation.go
@@ -5,7 +5,7 @@ import (
 )
 
 func evalRPN(tokens []string) int {
-	var operatorStack []int
+	var nums []int
 	operationSet := make(map[string]struct{})
 	for _, token := range "+-*/" {
 		operationSet[string(token)] = struct{}{}
@@ -13,11 +13,11 @@ func evalRPN(tokens []string) int {
 	for _, token := range tokens {
 		if _, ok := operationSet[token]; !ok {
 			tokenInInt, _ := strconv.Atoi(token)
-			operatorStack = append(operatorStack, tokenInInt)
+			nums = append(nums, tokenInInt)
 		} else {
-			left := operatorStack[len(operatorStack)-2]
-			right := operatorStack[len(operatorStack)-1]
-			operatorStack = operatorStack[:len(operatorStack)-2]
+			left := nums[len(nums)-2]
+			right := nums[len(nums)-1]
+			nums = nums[:len(nums)-2]
 			var newOperator int
 			switch token {
 			case "+":
@@ -29,8 +29,8 @@ func evalRPN(tokens []string) int {
 			case "/":
 				newOperator = left / right
 			}
-			operatorStack = append(operatorStack, newOperator)
+			nums = append(nums, newOperator)
 		}
 	}
-	return operatorStack[0]
+	return nums[0]
 }
