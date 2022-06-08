65ac48cb1ba08e810b88b3a66958e1b2e2234e41
还行吧 <XiaoqingLee@users.noreply.github.com>
2022-06-08 20:09:41 +0800

    daily

---
+++ problems/jz0036_二叉搜索树与双向链表.py
@@ -0,0 +1,50 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+"""
+
+
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def treeToDoublyList(self, root: Node | None) -> Node | None:
+        if root is None:
+            return root
+
+        inorder_nodes: list[Node] = []
+
+        def traverse(node: Node | None) -> None:
+            if node is None:
+                return
+            traverse(node.left)
+            inorder_nodes.append(node)
+            traverse(node.right)
+
+        traverse(root)
+
+        head: Node | None = None
+        for i, node in enumerate(inorder_nodes):
+            if i == 0:
+                head = node
+                node.left = node
+                node.right = node
+            else:
+                last_node: Node = head.left
+
+                node.right = head
+                node.left = last_node
+
+                last_node.right = node
+
+                head.left = node
+
+        return head
