d0277112d5f9614025da44bcd61148f3f3f7cb98
还行吧 <XiaoqingLee@users.noreply.github.com>
2022-06-10 02:31:00 +0800

    daily

--- main.go
+++ main.go
@@ -1,15 +1,19 @@
 package main
 
-import (
-	"github.com/davecgh/go-spew/spew"
-)
-
 func main() {
 	//nums := []int{2, 7, 11, 15}
 	//target := 9
 	//fmt.Printf("%v\n", problems.TwoSum(nums, target))
-	spew.Dump('7')
-	spew.Dump(string('7'))
-	spew.Dump(rune(80))
-	spew.Dump(80)
+
+	//spew.Dump('7')
+	//spew.Dump(string('7'))
+	//spew.Dump(rune(80))
+	//spew.Dump(80)
+
+	//m := make(map[int][]int, 5)
+	//spew.Dump(m)
+	//spew.Dump(m[15])
+	//spew.Dump(m)
+	//m[15] = append(m[15], -1)
+	//spew.Dump(m)
 }
