488404b99275782769c675f964cb93741e67b849
还行吧 <XiaoqingLee@users.noreply.github.com>
2022-06-16 00:38:48 +0800

    daily

---
+++ basics/sort/radix_sort/array.py
@@ -0,0 +1,91 @@
+# 一. 整数排序
+# 1. LSD（Least significant digital）：排序方式由数值的最右边（低位）开始
+def radix_sort_lsd(nums: list[int]) -> list[int]:
+    if len(nums) == 0:
+        return nums
+
+    num_strings: list[str] = [str(num) for num in nums]
+    digit_count = max(len(num_string) for num_string in num_strings)
+
+    def get_lowest_nth_digit(string: str, nth: int) -> str:
+        if len(string) - nth < 0:
+            return '0'
+        return string[-nth]
+
+    bucket_keys = "0123456789"
+    buckets: dict[str, list[str]] = {key: [] for key in bucket_keys}
+
+    for nth_digit in range(1, digit_count + 1):
+        for num_string in num_strings:
+            buckets[get_lowest_nth_digit(num_string, nth_digit)].append(num_string)
+        num_strings = []
+        for key in bucket_keys:
+            num_strings.extend(buckets[key])
+            buckets[key] = []
+
+    return [int(num_string) for num_string in num_strings]
+
+
+# 2. MSD（Most significant digital）：由数值的最左边（高位）开始。
+# 2.(1) 使用递归
+def radix_sort_msd(nums: list[int]) -> list[int]:
+    if len(nums) == 0:
+        return nums
+
+    num_strings: list[str] = [str(num) for num in nums]
+    digit_count = max(len(num_string) for num_string in num_strings)
+
+    def get_lowest_nth_digit(string: str, nth: int) -> str:
+        if len(string) - nth < 0:
+            return '0'
+        return string[-nth]
+
+    def get_highest_nth_digit(string: str, nth: int) -> str:
+        return get_lowest_nth_digit(string, digit_count + 1 - nth)
+
+    bucket_keys = "0123456789"
+
+    def sort(sorted_n_digits: int, strings: list[str]) -> list[str]:
+        if sorted_n_digits == digit_count:
+            return strings
+        buckets: dict[str, list[str]] = {key: [] for key in bucket_keys}
+        for num_string in strings:
+            buckets[get_highest_nth_digit(num_string, sorted_n_digits + 1)].append(num_string)
+        strings = []
+        for key in bucket_keys:
+            strings.extend(sort(sorted_n_digits + 1, buckets[key]))
+        return strings
+
+    num_strings = sort(0, num_strings)
+    return [int(num_string) for num_string in num_strings]
+
+
+# 2.(2) 使用Trie树
+# 将数字整形成等长字符串，从高位向低位构建单词，将单词逐一插入Trie树中，略。
+
+# 二. 单词排序 (符合 MSD 情景，使用Trie树, 排序结果即是先序遍历结果)
+def radix_sort_msd_using_trie(words: list[str]) -> list[str]:
+    from pypkg.datatype import Trie
+    trie: Trie = Trie()
+    for word in words:
+        trie.insert(word)
+
+    sorted_words: list[str] = []
+
+    def traverse(trie: Trie | None) -> None:
+        if trie is None:
+            return
+        if trie.is_leaf:
+            for n_times in range(trie.value_count):
+                sorted_words.append(trie.value)
+        for child in trie.children:
+            traverse(child)
+
+    traverse(trie)
+    return sorted_words
+
+
+if __name__ == '__main__':
+    print(radix_sort_lsd([1, 124, 35, 4]))
+    print(radix_sort_msd([1, 124, 35, 4]))
+    print(radix_sort_msd_using_trie(["leef", "leetcode", "leef", "leet", "michael", "leave", "alike"]))
--- pypkg/datatype.py
+++ pypkg/datatype.py
@@ -89,3 +89,116 @@ class PriorityQueue:
     def insert(self, element: int) -> None:
         self.__elements.append(element)
         self.__swim_up(self.size() - 1)
+
+
+# 叶子节点的数据为全null数组
+# 叶子节点代表的字符串的最后一个字符是什么这个信息存在parent的children数组里面
+# 一个空树只有一个root节点，self即是root
+class Trie:
+    ARRAY_LEN = 26
+
+    def __init__(self):
+        self.is_leaf: bool = False
+        self.value: str | None = None
+        self.value_count: int = 0
+        self.children: list[Trie | None] = [None] * self.ARRAY_LEN
+
+    def insert(self, word: str) -> None:
+        node: Trie = self
+        for char in word:
+            child: Trie | None = node.children[ord(char) - ord('a')]
+            if child is None:
+                print("inserting", char)
+                child = Trie()
+                node.children[ord(char) - ord('a')] = child
+            node = child
+        if node.is_leaf:
+            node.value_count += 1
+        else:
+            node.is_leaf = True
+            node.value = word
+            node.value_count = 1
+
+    def __delete(self, word: str) -> bool:
+        node_is_kept: bool = True
+        if word == '':
+            if self.is_leaf is False:
+                raise "word not in trie"
+            if self.value_count > 1:
+                self.value_count -= 1
+            elif self.value_count == 1:
+                self.value = None
+                self.value_count = 0
+                node_is_kept = False
+            return node_is_kept
+        child: Trie | None = self.children[ord(word[0])-ord('a')]
+        if child is None:
+            raise "word not in trie"
+        child_is_kept: bool = child.__delete(word[1:])
+        print("deleted", word[0], "node kept", child_is_kept)
+        if child_is_kept:
+            return node_is_kept
+        self.children[ord(word[0])-ord('a')] = None
+        if self.is_leaf is False and all(x is None for x in self.children):
+            node_is_kept = False
+            return node_is_kept
+        return node_is_kept
+
+    def delete(self, word: str) -> None:
+        self.__delete(word)
+
+    def search(self, word: str) -> bool:
+        exists: bool = False
+        node: Trie = self
+        for char in word:
+            node: Trie | None = node.children[ord(char) - ord('a')]
+            if node is None:
+                return exists
+        if node.is_leaf:
+            exists = True
+            return exists
+        return exists
+
+    def startsWith(self, prefix: str) -> bool:
+        exists: bool = False
+        node: Trie = self
+        for char in prefix:
+            node: Trie | None = node.children[ord(char) - ord('a')]
+            if node is None:
+                return exists
+        exists = True
+        return exists
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
+
+if __name__ == '__main__':
+    obj = Trie()
+    print('''obj.insert("leetcode")''')
+    obj.insert("leetcode")
+    print('''obj.insert("leef")''')
+    obj.insert("leef")
+    print('''obj.insert("leet")''')
+    obj.insert("leet")
+    print('''obj.search("leef")''')
+    print(obj.search("leef"))
+    print('''obj.search("leetcode")''')
+    print(obj.search("leetcode"))
+
+    print('''obj.delete("leetcode")=============================''')
+    obj.delete("leetcode")
+
+    print('''obj.search("leef")=================================''')
+    print(obj.search("leef"))
+    print('''obj.search("leetcode")''')
+    print(obj.search("leetcode"))
+    print('''obj.startsWith("l")''')
+    print(obj.startsWith("l"))
+    print('''obj.startsWith("")''')
+    print(obj.startsWith(""))
+    print('''obj.search("")''')
+    print(obj.search(""))
