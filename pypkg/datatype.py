d50389396d227e64c35f24adc5eb0852646065d8
还行吧 <XiaoqingLee@users.noreply.github.com>
2022-06-19 03:30:31 +0800

    daily

--- pypkg/datatype.py
+++ pypkg/datatype.py
@@ -188,45 +188,44 @@ class Trie:
         return exists
 
 
-# Your Trie object will be instantiated and called as such:
-# obj = Trie()
-# obj.insert(word)
-# param_2 = obj.search(word)
-# param_3 = obj.startsWith(prefix)
+class CircularQueue:
+    def __init__(self, capacity: int):
+        self.__capacity: int = capacity
+        self.__len: int = 0  # 需要这个变量来区分首尾指针重合在一起的情况是空状态还是满状态
+        self.__lst: list[int | None] = [None] * capacity
+        self.__index_to_write_to: int = 0  # 走得快，是队尾
+        self.__index_to_read_from: int = 0  # 走得慢，是队头
 
-if __name__ == '__main__':
-    obj = Trie()
-    print("=============================")
-    print('''obj.insert("leetcode")''')
-    obj.insert("leetcode")
-    print('''obj.insert("leef")''')
-    obj.insert("leef")
-    print('''obj.insert("leet")''')
-    obj.insert("leet")
-    print('''obj.search("leetcode")''')
-    print(obj.search("leetcode"))
-    print('''obj.search("leef")''')
-    print(obj.search("leet"))
-    print('''obj.search("leet")''')
-    print(obj.search("leef"))
+    def __len__(self):
+        return self.__len
 
-    print("=============================")
-    print('''obj.delete("leetcode")''')
-    obj.delete("leetcode")
-    print('''obj.delete("leet")''')
-    obj.delete("leet")
+    def enqueue(self, value: int) -> bool:
+        if self.is_full():
+            return False
+        self.__lst[self.__index_to_write_to] = value
+        self.__index_to_write_to = (self.__index_to_write_to + 1) % self.__capacity
+        self.__len += 1
+        return True
 
-    print("=============================")
-    print('''obj.search("leetcode")''')
-    print(obj.search("leetcode"))
-    print('''obj.search("leef")''')
-    print(obj.search("leet"))
-    print('''obj.search("leet")''')
-    print(obj.search("leef"))
+    def dequeue(self) -> bool:
+        if self.is_empty():
+            return False
+        self.__index_to_read_from = (self.__index_to_read_from + 1) % self.__capacity
+        self.__len -= 1
+        return True
 
-    print('''obj.startsWith("l")''')
-    print(obj.startsWith("l"))
-    print('''obj.startsWith("")''')
-    print(obj.startsWith(""))
-    print('''obj.search("")''')
-    print(obj.search(""))
+    def front(self) -> int:
+        if self.is_empty():
+            raise "Empty Queue"
+        return self.__lst[self.__index_to_read_from]
+
+    def rear(self) -> int:
+        if self.is_empty():
+            raise "Empty Queue"
+        return self.__lst[(self.__index_to_write_to - 1 + self.__capacity) % self.__capacity]
+
+    def is_empty(self) -> bool:
+        return self.__len == 0
+
+    def is_full(self) -> bool:
+        return self.__len == self.__capacity
