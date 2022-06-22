a7412f3d81d448ab5b069ca937f54f99f4d97fd7
还行吧 <XiaoqingLee@users.noreply.github.com>
2022-06-23 03:40:25 +0800

    update redis zset

--- main.go
+++ main.go
@@ -1,12 +1,8 @@
 package main
 
 func main() {
-	//nums := []int{2, 7, 11, 15}
-	//target := 9
-	//fmt.Printf("%v\n", problems.TwoSum(nums, target))
 
 	//spew.Dump('7')
 
 	//basics.TestBankersAlgorithm()
-
 }
