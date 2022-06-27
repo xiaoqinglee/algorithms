838e4c08bf6f0cba20a9994ca0ae425598a77cee
还行吧 <XiaoqingLee@users.noreply.github.com>
2022-06-28 03:39:58 +0800

    daily

--- basics/data_structure/UnionFind.py
+++ basics/data_structure/UnionFind.py
@@ -1,30 +1,28 @@
 from typing import Any
 
 
-ImmutableComparableElem = Any
+HashableComparableElem = Any
 
 
 class UnionFind:
     # 一个UnionFind中存储着多个集合，每个集合有一个代表，这个代表是这个集合的名片
     def __init__(self,
-                 from_vertexes: list[ImmutableComparableElem] | None = None,
-                 from_edges: list[tuple[ImmutableComparableElem, ImmutableComparableElem]] | None = None):
-        self.follows: dict[ImmutableComparableElem, ImmutableComparableElem] = {}
+                 from_vertexes: list[HashableComparableElem] | None = None,
+                 from_edges: list[tuple[HashableComparableElem, HashableComparableElem]] | None = None):
+        self.follows: dict[HashableComparableElem, HashableComparableElem] = {}
         if from_vertexes:
-            self.init_with_free_elements(from_vertexes)
+            self.init_with_free_vertexes(from_vertexes)
             return
         if from_edges:
-            self.init_with_linked_element_pairs(from_edges)
+            self.init_with_edges(from_edges)
             return
 
-    def init_with_free_elements(self, elems: list[ImmutableComparableElem]) -> None:
+    def init_with_free_vertexes(self, elems: list[HashableComparableElem]) -> None:
         for elem in elems:
             if elem not in self.follows:
                 self.follows[elem] = elem
 
-    def init_with_linked_element_pairs(self,
-                                       elem_pairs: list[tuple[ImmutableComparableElem, ImmutableComparableElem]]) \
-            -> None:
+    def init_with_edges(self, elem_pairs: list[tuple[HashableComparableElem, HashableComparableElem]]) -> None:
         for elem1, elem2 in elem_pairs:
             if elem1 not in self.follows:
                 self.follows[elem1] = elem1
@@ -33,11 +31,11 @@ class UnionFind:
             self.union(elem1, elem2)
 
     # 元素填充完毕之后才可以使用
-    def belongs_to_same_set(self, elem1: ImmutableComparableElem, elem2: ImmutableComparableElem) -> bool:
+    def belongs_to_same_set(self, elem1: HashableComparableElem, elem2: HashableComparableElem) -> bool:
         return self.find(elem1) == self.find(elem2)
 
     # 元素填充完毕之后才可以使用
-    def find(self, elem: ImmutableComparableElem) -> ImmutableComparableElem:
+    def find(self, elem: HashableComparableElem) -> HashableComparableElem:
         followed = self.follows[elem]
         if followed == elem:
             return elem
@@ -46,7 +44,7 @@ class UnionFind:
         return representative
 
     # 元素填充完毕之后才可以使用
-    def union(self, elem1: ImmutableComparableElem, elem2: ImmutableComparableElem) -> None:
+    def union(self, elem1: HashableComparableElem, elem2: HashableComparableElem) -> None:
         set1_rep = self.find(elem1)
         set2_rep = self.find(elem2)
         if set1_rep == set2_rep:
