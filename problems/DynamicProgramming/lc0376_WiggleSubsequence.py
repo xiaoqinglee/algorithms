896a4a83e622aca4092deb5408033083b9551c3a
还行吧 <XiaoqingLee@users.noreply.github.com>
2022-07-11 01:36:17 +0800

    daily

--- problems/DynamicProgramming/lc0376_WiggleSubsequence.py
+++ problems/DynamicProgramming/lc0376_WiggleSubsequence.py
@@ -1,18 +1,20 @@
 class Solution:
     def wiggleMaxLength(self, nums: list[int]) -> int:
 
-        # nums[i]作为子序列的最后一个元素且身份为山谷时最长的那个子序列[foo...i]的信息
+        # input example:
+        # 1 2 1 2 1 2 1 2 7 7 7 7 7 7 1 2
+        # 1 2 1 2 1 2 1 2 1 2 7 7 7 7 7 7
 
+        # nums[i]作为子序列的最后一个元素且身份为山谷时最长的那个子序列[foo...i]的信息 (可能有多个)
         # as_valley_is_nth_elem_in_seq[i]是这个子序列的长度
         as_valley_is_nth_elem_in_seq: list[int] = [-1] * len(nums)
-        # as_valley_prev_elem_idx[i]是这个子序列中倒第二个元素的索引
+        # as_valley_prev_elem_idx[i]是这个子序列中倒第二个元素的索引, 可以借助这个信息追溯其中一个满足条件的子序列
         as_valley_prev_elem_idx: list[int] = [-1] * len(nums)
 
-        # nums[i]作为子序列的最后一个元素且身份为山峰时最长的那个子序列[foo...i]的信息
-
+        # nums[i]作为子序列的最后一个元素且身份为山峰时最长的那个子序列[foo...i]的信息 (可能有多个)
         # as_peak_is_nth_elem_in_seq[i]是这个子序列的长度
         as_peak_is_nth_elem_in_seq: list[int] = [-1] * len(nums)
-        # as_peak_prev_elem_idx[i]是这个子序列中到第二个元素的索引
+        # as_peak_prev_elem_idx[i]是这个子序列中到第二个元素的索引, 可以借助这个信息追溯其中一个满足条件的子序列
         as_peak_prev_elem_idx: list[int] = [-1] * len(nums)
 
         as_valley_is_nth_elem_in_seq[0] = 1
@@ -39,11 +41,5 @@ class Solution:
                         max_seq_len = seq_len
                         as_valley_is_nth_elem_in_seq[j] = max_seq_len
                         as_valley_prev_elem_idx[j] = i
-
-        last_elem_is_peak: bool = True
-        longest_subsequence_len = as_peak_is_nth_elem_in_seq[len(nums) - 1]
-        if as_valley_is_nth_elem_in_seq[len(nums) - 1] > as_peak_is_nth_elem_in_seq[len(nums) - 1]:
-            last_elem_is_peak: bool = False
-            longest_subsequence_len = as_valley_is_nth_elem_in_seq[len(nums) - 1]
 
         return max(max(as_valley_is_nth_elem_in_seq), max(as_peak_is_nth_elem_in_seq))
