5c01a5cb5ebb341e2626b5fac6b5a2ca33df5bec
还行吧 <XiaoqingLee@users.noreply.github.com>
2022-07-10 21:59:15 +0800

    daily

--- problems/String/lc1044_longestDuplicateSubstring.py
+++ problems/String/lc1044_longestDuplicateSubstring.py
@@ -6,7 +6,26 @@ class Solution:
     # naive 的做法是把所有的子串都放到字典{substring: count}里，然后遍历出来
 
     def longestDupSubstring(self, s: str) -> str:
-        suffix_array = SuffixArray(s)
+        suffix_array: SuffixArray = SuffixArray(s)
+
+        from pprint import pprint
+        pprint(suffix_array.suffix_array)
+        pprint(suffix_array.suffix_start_index_array)
+        pprint(suffix_array.longest_common_prefix_length_array)
+
+        substrings: list[str] = []
+        for i, suffix_start_index in enumerate(suffix_array.suffix_start_index_array):
+            for j in range(1, len(suffix_array.suffix_array[i])+1):
+                substrings.append(suffix_array.suffix_array[i][:j])
+        print("substrings:")
+        print(substrings)
+
+        dup_substrings: list[str] = []
+        for i, len_ in enumerate(suffix_array.longest_common_prefix_length_array):
+            for j in range(1, len_+1):
+                dup_substrings.append(suffix_array.suffix_array[i][:j])
+        print("dup_substrings:")
+        print(dup_substrings)
 
         max_len = -float("inf")
         max_len_dup_substring = ""
@@ -15,10 +34,8 @@ class Solution:
                 max_len = len_
                 max_len_dup_substring = suffix_array.suffix_array[i][:len_]
 
-        dup_substrings: list[str] = []
-        for i, len_ in enumerate(suffix_array.longest_common_prefix_length_array):
-            for j in range(1, len_+1):
-                dup_substrings.append(suffix_array.suffix_array[i][:j])
-        print(dup_substrings)
-
         return max_len_dup_substring
+
+
+if __name__ == '__main__':
+    print(Solution().longestDupSubstring("AZAZA"))
