84a2e016cbcdfc63534204dece295470bc7e044e
还行吧 <XiaoqingLee@users.noreply.github.com>
2022-07-16 12:42:51 +0800

    daily

---
+++ problems/HashTable/lc0013_RomantoInteger.go
@@ -0,0 +1,33 @@
+package HashTable
+
+func romanToInt(s string) int {
+	symbolToValue := map[string]int{
+		"I":  1,
+		"V":  5,
+		"X":  10,
+		"IV": 4,
+		"IX": 9,
+		"L":  50,
+		"C":  100,
+		"XL": 40,
+		"XC": 90,
+		"D":  500,
+		"M":  1000,
+		"CD": 400,
+		"CM": 900,
+	}
+
+	result := 0
+
+	for currentIdx := 0; currentIdx <= len(s)-1; {
+		if currentIdx <= len(s)-2 && symbolToValue[s[currentIdx:currentIdx+2]] != 0 {
+			result += symbolToValue[s[currentIdx:currentIdx+2]]
+			currentIdx += 2
+		} else {
+			result += symbolToValue[s[currentIdx:currentIdx+1]]
+			currentIdx += 1
+		}
+	}
+
+	return result
+}
