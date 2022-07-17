0baf8a197aa308b2a91eba58c6d92fc5443b5a3b
还行吧 <XiaoqingLee@users.noreply.github.com>
2022-07-18 07:28:17 +0800

    daily

---
+++ problems/HashTable/lc0012_IntegertoRoman.go
@@ -0,0 +1,41 @@
+package HashTable
+
+import (
+	"sort"
+	"strings"
+)
+
+func intToRoman(num int) string {
+	valueToSymbol := map[int]string{
+		1:    "I",
+		5:    "V",
+		10:   "X",
+		4:    "IV",
+		9:    "IX",
+		50:   "L",
+		100:  "C",
+		40:   "XL",
+		90:   "XC",
+		500:  "D",
+		1000: "M",
+		400:  "CD",
+		900:  "CM",
+	}
+
+	values := make([]int, 0, len(valueToSymbol))
+	for key, _ := range valueToSymbol {
+		values = append(values, key)
+	}
+	sort.Stable(sort.Reverse(sort.IntSlice(values)))
+
+	var result []string
+	for _, value := range values {
+		count := num / value
+		num = num - count*value
+		for i := 0; i < count; i++ {
+			result = append(result, valueToSymbol[value])
+		}
+	}
+
+	return strings.Join(result, "")
+}
