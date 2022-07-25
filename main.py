fdd3c081a4449e85c40621a05e8744850504c340
还行吧 <XiaoqingLee@users.noreply.github.com>
2022-07-25 21:05:34 +0800

    daily

--- main.py
+++ main.py
@@ -1,2 +1,36 @@
+def merge_unique(array1: list[int], array2: list[int]) -> list[int]:
+    if len(array1) == 0:
+        return array2
+    if len(array2) == 0:
+        return array1
+
+    new_array: list[int] = []
+
+    array1_next: int = 0
+    array2_next: int = 0
+
+    while array1_next <= len(array1) - 1 and array2_next <= len(array2) - 1:
+        if array1[array1_next] < array2[array2_next]:
+            new_array.append(array1[array1_next])
+            array1_next += 1
+        elif array1[array1_next] > array2[array2_next]:
+            new_array.append(array2[array2_next])
+            array2_next += 1
+        else:
+            new_array.append(array1[array1_next])
+            array1_next += 1
+            array2_next += 1
+    while array1_next <= len(array1) - 1:
+        new_array.append(array1[array1_next])
+        array1_next += 1
+    while array2_next <= len(array2) - 1:
+        new_array.append(array2[array2_next])
+        array2_next += 1
+
+    return new_array
+
+
 if __name__ == '__main__':
-    pass
+    a1 = [9, 22, 35, 72]
+    a2 = [2, 14, 22, 35, 41, 42]
+    print(merge_unique(a1, a2))
---
+++ problems/Design/lc0355_DesignTwitter.py
@@ -0,0 +1,39 @@
+from collections import deque, defaultdict
+from collections.abc import Iterable, Iterator
+from itertools import count, islice
+import heapq
+
+
+class Twitter:
+
+    def __init__(self):
+        self.desc_timestamp_gen: Iterator[int] = count(start=0, step=-1)
+        # deque 元素: tuple[tweet_timestamp, tweet_id]
+        self.user_to_tweets: defaultdict[int, deque[tuple[int, int]]] = defaultdict(deque)
+        # fan_to_stars
+        self.user_to_followee: defaultdict[int, set[int]] = defaultdict(set)
+
+    def postTweet(self, user_id: int, tweet_id: int) -> None:
+        self.user_to_tweets[user_id].appendleft((next(self.desc_timestamp_gen), tweet_id))
+
+    def getNewsFeed(self, user_id: int) -> list[int]:
+        tweets_gen: Iterable[int] = heapq.merge(
+            *(self.user_to_tweets[u] for u in (self.user_to_followee[user_id] | {user_id})))
+        return [tweet for tweet_timestamp, tweet in islice(tweets_gen, 10)]
+
+    def follow(self, follower_id: int, followee_id: int) -> None:
+        if followee_id == follower_id:
+            return
+        self.user_to_followee[follower_id].add(followee_id)
+
+    def unfollow(self, follower_id: int, followee_id: int) -> None:
+        if followee_id not in self.user_to_followee[follower_id]:
+            return
+        self.user_to_followee[follower_id].remove(followee_id)
+
+# Your Twitter object will be instantiated and called as such:
+# obj = Twitter()
+# obj.postTweet(userId,tweetId)
+# param_2 = obj.getNewsFeed(userId)
+# obj.follow(followerId,followeeId)
+# obj.unfollow(followerId,followeeId)
