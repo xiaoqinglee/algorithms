8e1a794e8958e762941974788bc476f4b6089e8f
还行吧 <XiaoqingLee@users.noreply.github.com>
2022-07-29 21:22:20 +0800

    fuck you huawei

--- problems/HJ/001.py
+++
@@ -1,9 +0,0 @@
-def last_word_len(sentence: str) -> int:
-    i = len(sentence) - 1
-    while sentence[i] != " " and i >= 0:
-        i -= 1
-    return len(sentence) - 1 - i
-
-
-if __name__ == '__main__':
-    print(last_word_len(input()))
