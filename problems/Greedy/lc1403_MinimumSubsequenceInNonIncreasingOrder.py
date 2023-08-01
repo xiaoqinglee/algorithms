37236ee4588768354ab1e06714033f462ccf88d0
lixiaoqing <xiaoqinglee@foxmail.com>
2023-08-01 19:24:04 +0800

    daily

---
+++ problems/Greedy/lc1403_MinimumSubsequenceInNonIncreasingOrder.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minSubsequence(self, nums: list[int]) -> list[int]:
+        nums = sorted(nums, reverse=True)
+        sum_= 0
+        sum_of_the_rest= sum(nums)
+        for i, num in enumerate(nums):
+            sum_ += num
+            sum_of_the_rest -= num
+            if sum_ > sum_of_the_rest:
+                return nums[:i+1]
