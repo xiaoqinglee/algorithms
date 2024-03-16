75f7ec0d5e8cbd6c9ef9383da9ad3cba493e5e2e
lixiaoqing <xiaoqinglee@foxmail.com>
2024-03-17 01:02:51 +0800

    daily

---
+++ src/Map/lc0049_GroupAnagrams.exs
@@ -0,0 +1,11 @@
+defmodule Solution do
+  @spec group_anagrams(strs :: [String.t()]) :: [[String.t()]]
+  def group_anagrams(strs) do
+    strs
+    |> Enum.reduce(%{}, fn str, map ->
+      key = str |> String.to_charlist() |> Enum.sort() |> List.to_string()
+      Map.update(map, key, [str], &[str | &1])
+    end)
+    |> Map.values()
+  end
+end
