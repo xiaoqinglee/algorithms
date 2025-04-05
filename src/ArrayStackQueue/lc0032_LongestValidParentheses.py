b5510cd7005e236df7f72fe961adc5d61958be14
lixiaoqing <xiaoqinglee@foxmail.com>
2025-04-05 23:10:54 +0800

    update

--- src/ArrayStackQueue/lc0032_LongestValidParentheses.py
+++ src/ArrayStackQueue/lc0032_LongestValidParentheses.py
@@ -11,16 +11,11 @@ class Solution:
         stack: list[tuple[Char, int]] = []
 
         for i, char in enumerate(s):
-            if char == "(":
-                stack.append((char, i))
-            elif char == ")":
-                if len(stack) == 0 or stack[-1][0] != "(":
-                    stack.append((char, i))
-                else:  # stack[-1][0] == "("
-                    stack.pop()
-                    length = i - (stack[-1][1] if len(stack) > 0 else -1)
-                    max_len = max(length, max_len)
+            if char == ")" and (len(stack) > 0 and stack[-1][0] == "("):
+                stack.pop()
+                length = i - (stack[-1][1] if len(stack) > 0 else -1)
+                max_len = max(length, max_len)
             else:
-                raise "Invalid Input"
+                stack.append((char, i))
 
         return max_len
