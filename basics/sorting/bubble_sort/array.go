3231cb8ec892ff1d7100ee403fe4f2c41b0e42f5
lixiaoqing <xiaoqinglee@foxmail.com>
2025-04-06 23:40:46 +0800

    update

--- basics/sorting/bubble_sort/array.go
+++ basics/sorting/bubble_sort/array.go
@@ -3,7 +3,7 @@ package bubble_sort
 func BubbleSort(array []int) { //使得右边的有序子数组在其左边界慢慢膨胀
 	for floatedElementCount := 0; floatedElementCount < len(array)-1; floatedElementCount++ {
 		isChanged := false
-		for j := 0; j <= len(array)-2-floatedElementCount; j++ {
+		for j := 0; j+1 <= len(array)-1-floatedElementCount; j++ {
 			if array[j+1] < array[j] { // 冒泡排序应当是稳定的
 				array[j], array[j+1] = array[j+1], array[j]
 				isChanged = true
