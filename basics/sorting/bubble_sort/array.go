1118859e7997923ece228376794174f703ad2c8e
lixiaoqing <xiaoqinglee@foxmail.com>
2025-04-15 13:09:23 +0800

    update

--- basics/sorting/bubble_sort/array.go
+++ basics/sorting/bubble_sort/array.go
@@ -4,7 +4,7 @@ func BubbleSort(array []int) { //使得右边的有序子数组在其左边界�
 	for floatedElementCount := 0; floatedElementCount < len(array)-1; floatedElementCount++ {
 		isChanged := false
 		for j := 0; j+1 <= len(array)-1-floatedElementCount; j++ {
-			if array[j+1] < array[j] { // 冒泡排序应当是稳定的
+			if array[j] > array[j+1] { // 冒泡排序应当是稳定的
 				array[j], array[j+1] = array[j+1], array[j]
 				isChanged = true
 			}
