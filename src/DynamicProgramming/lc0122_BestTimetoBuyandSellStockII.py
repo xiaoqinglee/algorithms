9101678360b5f4b742dab91a6665de8b651c043b
lixiaoqing <xiaoqinglee@foxmail.com>
2025-04-17 19:10:43 +0800

    update

--- src/DynamicProgramming/lc0122_BestTimetoBuyandSellStockII.py
+++ src/DynamicProgramming/lc0122_BestTimetoBuyandSellStockII.py
@@ -2,7 +2,7 @@
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
 
-        # dp[i][...] 为第i（0 <= i <= len(prices - 1)）天闭市时手中持股与否对应的最大资产。
+        # dp[i][...] 为第i（0 <= i <= len(prices - 1)）天闭市时手中持股与否对应的最大现金数。
         # dp[i][0] 代表天闭市时手中不持股（又分为当天没动和当天售出两个情况）：
         #       dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i])
         # dp[i][1] 代表天闭市时手中持股（又分为当天没动和当买入出两个情况）：
